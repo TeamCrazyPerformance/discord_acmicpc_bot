631075601b453ffad24ae9e81ccc83e8c981e147
wiselight <company@wiselight.kr>
2022-10-03 17:50:13 +0900

    feat: 제출 내역 모델 생성

--- acmicpc/models.py
+++ acmicpc/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Member(models.Model):
     user_id = models.CharField(max_length=20, primary_key=True)
     solved = models.IntegerField()
+
+
+class Submit(models.Model):
+    submit_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey("Member", related_name="submit", on_delete=models.CASCADE, db_column="user_id")
+    result = models.CharField(max_length=100)
